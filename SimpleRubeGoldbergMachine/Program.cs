521bb334577786c3fda3b93f04d3ef30c4b08918
Jarmanso7 <jordi.armangue@hotmail.com>
2020-04-25 21:31:41 +0200

    ~ Fourth scenario. 1 ball, 2 rows of 5 domino tokens and 2 bells

--- SimpleRubeGoldbergMachine/Program.cs
+++ SimpleRubeGoldbergMachine/Program.cs
@@ -7,7 +7,7 @@
             //Setup the elements of the RubeGoldbergMachine
             var ball = new Ball();
 
-            var dominoTokens = new[]
+            var leftRowOfDominoes = new[]
             {
                 new DominoToken(),
                 new DominoToken(),
@@ -16,20 +16,36 @@
                 new DominoToken()
             };
 
-            var bell = new Bell();
+            var leftBell = new Bell();
 
-            //target the first domino token with the ball
-            ball.Collision += dominoTokens[0].Collided;
+            var rightRowOfDominoes = new[]
+            {
+                new DominoToken(),
+                new DominoToken(),
+                new DominoToken(),
+                new DominoToken(),
+                new DominoToken()
+            };
 
-            //Set the dominoes in a row so that
+            var rightBell = new Bell();
+
+            //target the first domino token of each row with the same ball
+            ball.Collision += leftRowOfDominoes[0].Collided;
+            ball.Collision += rightRowOfDominoes[0].Collided;
+
+            //Set the dominoes of each row so that
             //when each token falls, it triggers the fall of the next one.
             for (var i = 0; i < 4; i++)
             {
-                dominoTokens[0].Fall += dominoTokens[i + 1].Collided;
+                leftRowOfDominoes[0].Fall += leftRowOfDominoes[i + 1].Collided;
+                rightRowOfDominoes[0].Fall += rightRowOfDominoes[i + 1].Collided;
             }
 
-            //Set the bell right after the last domino token
-            dominoTokens[4].Fall += bell.Collided; 
+            //Set the bell right after the last domino token of the left row
+            leftRowOfDominoes[4].Fall += leftBell.Collided;
+
+            //Set the bell right after the last domino token of the right row
+            rightRowOfDominoes[4].Fall += rightBell.Collided;
 
             //Kick-off
             ball.Roll();
