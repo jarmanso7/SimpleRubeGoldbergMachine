ea003669102cf571b5aa76f8823f6a2d9d9988d5
Jarmanso7 <jordi.armangue@hotmail.com>
2020-04-26 05:30:43 +0200

    ~ Added threads to DominoToken

--- SimpleRubeGoldbergMachine/DominoToken.cs
+++ SimpleRubeGoldbergMachine/DominoToken.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace SimpleRubeGoldbergMachine
 {
@@ -8,13 +9,21 @@ namespace SimpleRubeGoldbergMachine
 
         public void Collided(object sender, EventArgs e)
         {
-            var objectType = sender.GetType().Name;
-            Console.WriteLine($"A {objectType} has bumped into the domino token.");
-            Console.WriteLine("The token falls!");
+            var thread = new Thread(() =>
+            {
+                var objectType = sender.GetType().Name;
+                Console.WriteLine($"A {objectType} has bumped into the domino token.");
+                Console.WriteLine("Current Thread: " + Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(1000);
+                Console.WriteLine("The token falls!");
+                //On falling the domino token collides with the next object
+                this.OnFalling(EventArgs.Empty);
+            });
 
-            //On falling the domino token collides with the next object
-            OnFalling(EventArgs.Empty);
+            thread.IsBackground = false;
+            thread.Start();
         }
+
         private void OnFalling(EventArgs e)
         {
             Fall?.Invoke(this, e);
--- SimpleRubeGoldbergMachine/Program.cs
+++ SimpleRubeGoldbergMachine/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleRubeGoldbergMachine
+﻿using System;
+
+namespace SimpleRubeGoldbergMachine
 {
     public class Program
     {
