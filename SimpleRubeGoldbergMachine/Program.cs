9bdfcdcd11f1b2ea17dbc1b4fab7d9f6efa86bb3
Jarmanso7 <jordi.armangue@hotmail.com>
2020-05-01 20:18:18 +0200

    ~ Clean up SecondScenario

--- SimpleRubeGoldbergMachine/Program.cs
+++ SimpleRubeGoldbergMachine/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace SimpleRudeGoldbergMachine
+﻿namespace SimpleRubeGoldbergMachine
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            //Setup the elements of the RubeGoldbergMachine
+        
             var ball = new Ball();
             var dominoToken = new DominoToken();
             var bell = new Bell();
@@ -12,7 +12,6 @@
             ball.Collision += dominoToken.Collided;
             dominoToken.Fall += bell.Collided;
 
-            //Kick-off
             ball.Roll();
         }
     }
